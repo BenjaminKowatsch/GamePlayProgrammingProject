2190b487bfac3730919aa1ba9e76e154fd860535
Tobias Schwarz <tobias.schwarz1994@live.de>
2015-07-24 19:29:17 +0200

    - deleted unecessary Scripts

--- data/scripts/utils/physicsworld.lua
+++
@@ -1,9 +0,0 @@
-logMessage("using defaults/physicsWorld.lua")
-
-do -- Physics world
-	local cinfo = WorldCInfo()
-	cinfo.gravity = Vec3(0, 0, 0)
-	cinfo.worldSize = 2000.0
-	physicsWorld = PhysicsFactory:createWorld(cinfo)
-	PhysicsSystem:setWorld(physicsWorld)
-end
