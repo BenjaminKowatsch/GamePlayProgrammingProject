b989a0e7082c83461281b3f498e9f44a30a415c9
Tobias Schwarz <tobias.schwarz1994@live.de>
2015-07-24 19:58:52 +0200

    - finished level 1

--- data/scripts/SuperMonkeyBall/level1.lua
+++ data/scripts/SuperMonkeyBall/level1.lua
@@ -17,53 +17,27 @@ function Level1:create()
 	self.gameObjects[self.goal.go:getGuid()]=self.goal
 	--Double Jump
 	self.jump = DoubleJumpPickup()
-	self.jump:create("jump",Vec3(0,80,0),0x1,5,5,5,self)
+	self.jump:create("jump",Vec3(0,290,10),0x1,5,5,5,self)
 	self.gameObjects[self.jump.go:getGuid()]=self.jump
 	--Speed
 	self.speed = SpeedPickup()
-	self.speed:create("speed",Vec3(0,545,0),0x1,5,5,5,self, 10)
+	self.speed:create("speed",Vec3(50,554,10),0x1,5,5,5,self, 10)
 	self.gameObjects[self.speed.go:getGuid()]=self.speed
 	--Coins
 	self.coin1 = CoinPickup()
-	self.coin1:create("c1",Vec3(40,60,0),0x1,5,5,5,self)
+	self.coin1:create("c1",Vec3(0,554,10),0x1,5,5,5,self)
 	self.gameObjects[self.coin1.go:getGuid()]=self.coin1
 	self.coin2 = CoinPickup()
-	self.coin2:create("c2",Vec3(40,45,0),0x1,5,5,5,self)
+	self.coin2:create("c2",Vec3(0,425,31),0x1,5,5,5,self)
 	self.gameObjects[self.coin2.go:getGuid()]=self.coin2
 	self.coin3 = CoinPickup()	
-	self.coin3:create("c3",Vec3(40,30,0),0x1,5,5,5,self)
+	self.coin3:create("c3",Vec3(189,388,10),0x1,5,5,5,self)
 	self.gameObjects[self.coin3.go:getGuid()]=self.coin3
 	self.coin4 = CoinPickup()
-	self.coin4:create("c4",Vec3(40,15,0),0x1,5,5,5,self)
+	self.coin4:create("c4",Vec3(189,348,10),0x1,5,5,5,self)
 	self.gameObjects[self.coin4.go:getGuid()]=self.coin4
 	self.coin5 = CoinPickup()
-	self.coin5:create("c5",Vec3(40,0,0),0x1,5,5,5,self)
+	self.coin5:create("c5",Vec3(189,308,10),0x1,5,5,5,self)
 	self.gameObjects[self.coin5.go:getGuid()]=self.coin5
-	self.coin6 = CoinPickup()
-	self.coin6:create("c6",Vec3(40,60,0),0x1,5,5,5,self)
-	self.gameObjects[self.coin6.go:getGuid()]=self.coin6
-	self.coin7 = CoinPickup()
-	self.coin7:create("c7",Vec3(40,45,0),0x1,5,5,5,self)
-	self.gameObjects[self.coin7.go:getGuid()]=self.coin7
-	self.coin8 = CoinPickup()
-	self.coin8:create("c8",Vec3(40,60,0),0x1,5,5,5,self)
-	self.gameObjects[self.coin8.go:getGuid()]=self.coin8
-	self.coin9 = CoinPickup()
-	self.coin9:create("c9",Vec3(40,45,0),0x1,5,5,5,self)
-	self.gameObjects[self.coin9.go:getGuid()]=self.coin9
-	self.coin10 = CoinPickup()	
-	self.coin10:create("c10",Vec3(40,30,0),0x1,5,5,5,self)
-	self.gameObjects[self.coin10.go:getGuid()]=self.coin10
-	self.coin11 = CoinPickup()
-	self.coin11:create("c11",Vec3(40,15,0),0x1,5,5,5,self)
-	self.gameObjects[self.coin11.go:getGuid()]=self.coin11
-	self.coin12 = CoinPickup()
-	self.coin12:create("c12",Vec3(40,0,0),0x1,5,5,5,self)
-	self.gameObjects[self.coin12.go:getGuid()]=self.coin12
-	self.coin13 = CoinPickup()
-	self.coin13:create("c13",Vec3(40,60,0),0x1,5,5,5,self)
-	self.gameObjects[self.coin13.go:getGuid()]=self.coin13
-	self.coin14 = CoinPickup()
-	self.coin14:create("c14",Vec3(40,45,0),0x1,5,5,5,self)
-	self.gameObjects[self.coin14.go:getGuid()]=self.coin14	
+	
 end
